fc6a7cc6f0f3734defb6b1a6e0d088f41baa794c
derrick b <dbozich1@gmail.com>
2018-10-31 20:12:38 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,7 +1,7 @@
 export function manageFriends(state, action){
   switch (action.type){
     case 'ADD_FRIEND':
-      return {...state, friends: [...state.freinds, friend]}
+      return {...state, friends: [...state.friends, friend]}
     default:
       return state;
   }
